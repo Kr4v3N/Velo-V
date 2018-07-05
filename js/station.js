b75ed0f7617f5060e43fb3a0ba2cea84aea57059
Kr4v3N <fafachen37@gmail.com>
2018-07-05 10:01:26 +0200

    Implementation canvas-1

--- js/station.js
+++ js/station.js
@@ -1,48 +1,65 @@
-// ---------------------------- //
-// ------    STATION  --------- //
-// ---------------------------- //
+// CREATION DE L'OBJET STATION QUI RECUPERE ET AFFICHE LES INFOS D'UNE STATION VELOV
+var stationTitle = document.getElementById("station-titre");
+var statusStation = document.getElementById("station-ouverture");
+var addressStation = document.getElementById("station-adresse");
+var availableBike = document.getElementById("station-velo-dispo");
+var availableStand = document.getElementById("station-emplacement-dispo")
+var buttonConfirme = document.getElementById("bouton-valider");
+var buttonErase = document.getElementById("bouton-efface");
+var buttonReserve = document.getElementById("bouton-confirme");
+var canvas = document.getElementById("signature");
 
-// Création de l'objet station qui récupère et affiche les données d'une station
 var Station = {
-    // Fonction d'initialisation d'une station
-    initStation: function(station) {
-        this.name = station.name.split("-")[1]; // Extraction du nom de la station en faisant un découpage de la chaîne de caractères grâce à un split('-'), et je récupéres le premier élément après le tiret.
-        this.address = station.address;
-        if (station.status === "OPEN") {
-            this.status = "ouverte";
-        } else if (station.status === "CLOSED") {
-            this.status = "fermée";
-        }
-        this.availableStand = station.available_bike_stands;
-        this.availableBike = station.available_bikes;
-    },
-
-    //Fontion qui affiche les données d'une station.
-    decrireStation: function() {
-        // Je procéde aux testes sur la disponibilité des vélos.
-        if (this.status === "fermée") {
-            buttonReserve.style.display = "none";
-            availableBike.style.display = "none";
-            availableStand.style.display = "none";
-            statusStation.style.color = "red";
-        } else if (this.availableBike === 0) {
-            buttonReserve.style.display = "none";
-            availableBike.style.display = "inline";
-            availableBike.style.color = "red";
-            availableStand.style.display = "inline";
-            statusStation.style.color = "green";
-        } else {
-            buttonReserve.style.display = "flex";
-            availableBike.style.display = "inline";
-            availableStand.style.display = "inline";
-            statusStation.style.color = "green";
-            availableBike.style.color = "green";
-        }
-
-        stationTitle.textContent = "STATION : " + this.name;
-        addressStation.textContent = this.address;
-        statusStation.textContent = "Cette Station est actuellement : " + this.status;
-        availableBike.textContent = "Nombre de vélos disponibles : " + this.availableBike;
-        availableStand.textContent = "Emplacements Vides : " + this.availableStand;
-    }
+	// Fonction d'initialisation d'une station
+	initStation: function(station) {
+		this.name = station.name.split("- ")[1];
+		this.address = station.address;
+		if (station.status === "OPEN") {
+			this.status = "ouverte";
+		} else if (station.status === "CLOSED") {
+			this.status = "fermée";
+		}
+		this.availableStand = station.available_bike_stands;
+		this.availableBike = station.available_bikes;
+		// Si reservation en cours, alors un vélo en moins dans la station
+		if (typeof sessionStorage!='undefined') {
+			if ('time' in sessionStorage) {
+				if (sessionStorage.getItem("nomStation") === this.name) {
+					this.availableBike = station.available_bikes - 1;
+				}
+		    }
+		} else {
+			alert ("sessionStorage n'est pas supporté");
+		};
+	},
+	//FONCTION QUI AFFICHE LES INFOS D'UNE STATION
+	decrireStation: function() {
+		// FAIRE ICI LES TEST SUR LES DISPO VELO
+		if (this.status === "fermée") {
+			buttonReserve.style.display = "none";
+			availableBike.style.display = "none";
+			availableStand.style.display = "none";
+			statusStation.style.color = "red";
+		} else if (this.availableBike === 0) {
+			buttonReserve.style.display = "none";
+			availableBike.style.display = "inline";
+			availableBike.style.color = "red";
+			availableStand.style.display = "inline";
+			statusStation.style.color = "green";
+		} else {
+			buttonReserve.style.display = "flex";
+			availableBike.style.display = "inline";
+			availableStand.style.display = "inline";
+			statusStation.style.color = "green";
+			availableBike.style.color = "green";
+		}
+		canvas.style.display = "none";
+		buttonConfirme.style.display = "none";
+    	buttonErase.style.display = "none";
+		stationTitle.textContent = "STATION : " + this.name;
+		addressStation.textContent = this.address;
+		statusStation.textContent = "Cette Station est actuellement : " + this.status;
+		availableBike.textContent = "Nombre de vélos disponibles : " + this.availableBike;
+		availableStand.textContent = "Emplacements Vides : " + this.availableStand ;
+	}
 };
--- js/toggleNav.js
+++ js/toggleNav.js
@@ -1,25 +1,22 @@
-// ----------------------------------- //
-// ------  ANIMATION HAMBURGER  ------ //
-// ----------------------------------- //
-
-/* Le jQuery nous fournit également une méthode nous permettant de modifier l’état d'affectation d’un attribut class à un ou plusieurs éléments HTML, c’est-à-dire de l’ajouter si il n’est pas présent ou de le supprimer s'il l’est.*/
-
-/* Je place l'integralité de mon code jQuery à l'intérieur du gestionnaire d'évenement "ready" pour eviter tout problème d'execution du code à cause d'un chargement incomplet de la page.*/
+// Je place l'integralité de mon code jQuery à l'intérieur du gestionnaire d'évenement "ready" pour eviter tout problème d'execution du code à cause d'un chargement incomplet de la page.
 // $ appel à la librairie jQuery
 // $("selecteur css") puis avec le symbole "." j'applique une méthode à l'élément html.
 // $("selecteur css").methode()
 // J'utilise la méthode click pour gerer l'évenement de type click.
-/* Je passe dans la parenthèse de "function" un argument nommé "e" qui est l'équivalent de l'évènement "event".*/
-$(document).ready(function() { // Attente du chargement complet du DOM
+// Je passe dans la parenthèse de "function" un argument nommé "e" qui est l'équivalent de l'évènement "event".
 
-    $(".m-nav-toggle").click(function(e) {
-        e.preventDefault(); // La méthode preventDefault() de cet objet permet d’empêcher le navigateur de déclencher son comportement par défaut.(Dans ce cas-ci il n'est pas d'une grande utilité)
-        $(".m-nav-toggle").toggleClass("open"); // Animation des barres de l'hamburger.
-        $(".m-right").toggleClass("open"); // Animation de la navbar.
+ $(document).ready(function(){ // Attente du chargement complet du DOM
+
+    $(".m-nav-toggle").click(function(e){
+    e.preventDefault(); // La méthode preventDefault() de cet objet permet d’empêcher le navigateur de déclencher son comportement par défaut.
+    $(".m-nav-toggle").toggleClass("open"); // Animation des barres de l'hamburger.
+    $(".m-right").toggleClass("open"); // Animation de la navbar.
     })
 
-});
+ });
 
+// Le jQuery nous fournit également une méthode nous permettant de modifier l’état d'affectation d’un attribut class à un ou plusieurs éléments HTML, c’est-à-dire de l’ajouter si il n’est pas présent ou de le supprimer si il l’est.
 
-/* La méthode "toggleClass()"" va nous permettre d’inverser l’état de visibilité d’une classe, c’est à dire de l’afficher si il est caché ou de le cacher si il est affiché.*/
+// La méthode "toggleClass()"" va nous permettre d’inverser l’état de visibilité d’une classe, c’est à dire de l’afficher si il est caché ou de le cacher si il est affiché.
+
 
